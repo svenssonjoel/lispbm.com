a833c08d1364c46d78e2a0d5987c819b8291c257
Joel Svensson <svenssonjoel@yahoo.se>
2025-11-05 12:20:02 +0100

    update with VESC documentation

--- lispbm-reference-manual/src/fetch.sh
+++ lispbm-reference-manual/src/fetch.sh
@@ -6,3 +6,12 @@ cp ../../../lispbm/doc/dynref.md .
 cp ../../../lispbm/doc/lbmref.md .
 cp ../../../lispbm/doc/runtimeref.md .
 cp ../../../lispbm/doc/ttfref.md .
+
+# Get the  vesc express display manual
+wget https://raw.githubusercontent.com/vedderb/vesc_express/refs/heads/main/main/display/README.md
+mv README.md vesc-express-display.md
+
+# Get the vesc LispBM CORE reference manual 
+wget https://raw.githubusercontent.com/vedderb/bldc/refs/heads/master/lispBM/README.md
+mv README.md vesc-lisp-documentation.md
+
