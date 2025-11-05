a77530865df43fb5dbb2be1cbed4fc2c3ec60901
Joel Svensson <svenssonjoel@yahoo.se>
2025-11-05 10:30:42 +0100

    added recreated manuals

---
+++ lispbm-reference-manual/src/fetch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+cp ../../../lispbm/doc/displayref.md .
+cp ../../../lispbm/doc/dynref.md .
+cp ../../../lispbm/doc/lbmref.md .
+cp ../../../lispbm/doc/runtimeref.md .
+cp ../../../lispbm/doc/ttfref.md .
